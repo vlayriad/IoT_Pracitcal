a658573a623cf08163a5ef0f74cdd957e77267d0
vlayriad <sacvrr@gmail.com>
2023-11-05 03:18:00 +0700

    Tugas P1 Mencetak Tempratur dan Kelembapan

--- IoT_P1_DHT11_OUTPUT/src/main.cpp
+++ IoT_P1_DHT11_OUTPUT/src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
+#include "DHT.h" // Sertakan pustaka DHT
 
-// put function declarations here:
-int myFunction(int, int);
+#include "esp_log.h" // Sertakan pustaka ESP_LOG
+
+static const char *TAG = "DHT"; // Tag log khusus
+
+/* Konfigurasi PIN dan TIPE DHT11 */
+#define DHTPIN 4 // Sesuaikan pin input dengan yang Anda gunakan
+#define DHTTYPE DHT11 // Sesuaikan dengan jenis sensor yang digunakan
+
+DHT dht(DHTPIN, DHTTYPE); // Membuat objek DHT
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  ESP_LOGI(TAG, "Deteksi Suhu dan Kelembaban"); // Mencetak info log menggunakan ESP_LOG
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  delay(2000); // Jeda pembacaan sensor
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  float t = dht.readTemperature(); // Membaca suhu dari sensor
+  float h = dht.readHumidity(); // Membaca kelembaban dari sensor
+
+  if (isnan(h) || isnan(t)) {
+    ESP_LOGW(TAG, "Gagal membaca data dari sensor DHT."); // Log peringatan jika gagal membaca data
+    return;
+  }
+
+  ESP_LOGI(TAG, "Suhu: %.2f °C, Kelembaban: %.2f %%", t, h); // Mencetak suhu dan kelembaban dengan dua desimal
 }
